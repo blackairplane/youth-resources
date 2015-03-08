d1b2f435af1bf4825b363f48bcae3d0092b76a0e
Jamie Howard <jamie@blackairplane.com>
2015-03-07 20:49:46 -0500

    Update the admin section

---
+++ plugins/pico_admin/admin_config.php
@@ -0,0 +1,11 @@
+<?php
+
+global $pico_admin_password;
+
+/* 
+ * This should be a sha1 hash of your password.
+ * Use a tool like http://www.sha1-online.com to generate.
+ */
+$pico_admin_password = '7288edd0fc3ffcbe93a0cf06e3568e28521687bc';
+
+?>
